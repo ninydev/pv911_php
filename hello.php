6860cc3091c94ab4093abe5557bbc490fadecef7
Olexander Nykytin <nikitin_a@itstep.academy>
2021-11-22 17:48:46 +0200

    Init

---
+++ hello.php
@@ -0,0 +1,24 @@
+<?php
+
+phpinfo();
+
+
+/*
+// create curl resource
+$ch = curl_init();
+
+// set url
+curl_setopt($ch, CURLOPT_URL, "https://www.php.net/");
+
+//return the transfer as a string
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+// $output contains the output string
+$output = curl_exec($ch);
+
+var_dump($output);
+
+// close curl resource to free up system resources
+curl_close($ch);
+
+*/
