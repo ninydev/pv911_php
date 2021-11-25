d71950a1b1a8f52cfd280d603a22f1b5b0c6e1a8
Olexander Nykytin <nikitin_a@itstep.academy>
2021-11-25 16:30:01 +0200

    function buildTable

--- simpleCMS/index.php
+++ simpleCMS/index.php
@@ -2,10 +2,14 @@
 
 require_once ("lib/buildTable.php");
 
+
+require_once ("template/header.php");
+
+
 $openPage = "index.php";
 if (isset($_GET['openPage'])) $openPage = $_GET['openPage'];
 if (isset($_POST['openPage'])) $openPage = $_POST['openPage'];
-
-require_once ("template/header.php");
 require_once ("pages/" . $openPage);
+
+
 require_once ("template/footer.php");
--- simpleCMS/lib/buildTable.php
+++ simpleCMS/lib/buildTable.php
@@ -1,11 +1,12 @@
 <?php
 
 
-function buildStandardTable () {
+function buildStandardTable (): string
+{
 
     $res = "<tbody>";
     for ($r = 0; $r < 10; $r++) {
-        $res.="<tr>";
+        $res.="\n<tr>";
         for ($c=0; $c < 10; $c++){
             $res.= "<td>" . $r . " | " . $c . "</td>";
         }
