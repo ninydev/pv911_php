f4879b869c63c527002c9247f426e2e41d46c370
Olexander Nykytin <nikitin_a@itstep.academy>
2021-12-06 17:47:13 +0200

    CRUD - базовые операции с данными MySQL

--- comp/public/index.php
+++ comp/public/index.php
@@ -2,7 +2,19 @@
 require_once (__DIR__ ."/../vendor/autoload.php");
 
 
-\ninydev\Controllers\EntityController::helloWorld();
+// # ninydev.Models.Entity.helloWorld();
+\ninydev\Models\Entity::helloWorld();
+
+// using ninydev.Models;
+// Entity.helloWorld();
+use ninydev\Models\Entity;
+Entity::helloWorld();
+
+
+
+
+// \ninydev\Controllers\EntityController::helloWorld();
+
 
 
 
--- comp/public/oldStyleMySql.php
+++ comp/public/oldStyleMySql.php
@@ -7,3 +7,35 @@ require_once (__DIR__ ."/../vendor/autoload.php");
 
 
 $mysqli = new mysqli(DB::$hostName, DB::$userName, DB::$password, DB::$database);
+// Check connection
+if ($mysqli -> connect_errno) {
+    echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
+    exit();
+}
+
+
+
+
+// CREATE
+$sqlStr = 'INSERT INTO  posts (title, body) VALUES ("title", "body")';
+$result = $mysqli->query($sqlStr);
+var_dump($result);
+
+// READ
+$sqlStr = "SELECT * FROM post";
+$result = $mysqli->query($sqlStr);
+var_dump($result);
+
+// UPDATE
+$sqlStr = "UPDATE `posts` SET `title`='New Title' WHERE `id` = 1";
+$result = $mysqli->query($sqlStr);
+var_dump($result);
+
+
+// DELETE
+$sqlStr = "DELETE FROM `posts` WHERE `id` = 10";
+$result = $mysqli->query($sqlStr);
+var_dump($result);
+
+
+
